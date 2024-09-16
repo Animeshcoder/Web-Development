496a8231d22008cdabd841abd47e8bea71237749
Animeshcoder <animeshawasthi006.com>
2024-09-16 18:03:06 +0530

    Initial commit

---
+++ chat-app/src/components/messageList.jsx
@@ -0,0 +1,16 @@
+import './messagelist.css';
+
+import React from 'react';
+import Message from './message';
+
+const MessageList = ({ messages }) => {
+  return (
+    <div className="message-list">
+      {messages.map((msg, index) => (
+        <Message key={index} sender={msg.sender} text={msg.text} />
+      ))}
+    </div>
+  );
+};
+
+export default MessageList;
