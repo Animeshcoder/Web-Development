1554aa3a94bda460bd786762c9f7fe3eb037dcc0
Animesh Awasthi <115880036+Animeshcoder@users.noreply.github.com>
2024-09-19 11:51:29 +0530

    Update channel1.jsx

--- Chating-App/src/components/channel1.jsx
+++ Chating-App/src/components/channel1.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ChatWindow from './ChatWindow';
 
 const Channel1 = () => {
-  return <ChatWindow selected="channel1" />;
+  return <ChatWindow selected="Channel_1" />;
 };
 
 export default Channel1;
