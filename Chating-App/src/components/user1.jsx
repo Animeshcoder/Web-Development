5ffb35c258e6d9fee58e452fe49ae3c5d1cf2a68
Animesh Awasthi <115880036+Animeshcoder@users.noreply.github.com>
2024-09-19 11:52:24 +0530

    Update user1.jsx

--- Chating-App/src/components/user1.jsx
+++ Chating-App/src/components/user1.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ChatWindow from './ChatWindow';
 
 const User1 = () => {
-  return <ChatWindow selected="user1" />;
+  return <ChatWindow selected="user_1" />;
 };
 
 export default User1;
